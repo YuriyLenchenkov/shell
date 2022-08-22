5632f0d937321d93256bfbca9a264508d3243808
YuriyLenchenkov <83726590+YuriyLenchenkov@users.noreply.github.com>
2022-08-22 11:38:11 +0300

    Update check_cert_expiration.sh

--- check_cert_expiration.sh
+++ check_cert_expiration.sh
@@ -3,3 +3,7 @@ ovpncerdate=$(cat cert_file | grep "Not After" | cut -c 13-)
 #echo $ovpncerdate
 #date -d "$ovpncerdate" +%Y%m%d
 expr $(date -d "$ovpncerdate" +%s) - $(date +%s)
+
+file=$(find /etc/openvpn/ -type f -name '*.conf'); ovpncerdate=$(grep "Not After" $file | cut -c 25-); expr $(date -d "$ovpncerdate" +%s) - $(date +%s)
+
+expr $(date -d "$(cat $(find /etc/openvpn -type f -name *.conf) | grep "Not After" | cut -c 25-)" +%s) - $(date +%s)
